e7df2d3b267b5f12813c2fcda778e70b9767cd3f
coffeemakingtoaster <coffeemakingtoaster@gmail.com>
2025-04-04 12:36:09 +0200

    feat(logging): enable readable logging

--- main.go
+++ main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"os"
+
+	ghapi "github.com/coffeemakingtoaster/dockerfile-extractor/pkg/gh-api"
 	reporetriever "github.com/coffeemakingtoaster/dockerfile-extractor/pkg/repo-retriever"
 	"github.com/coffeemakingtoaster/dockerfile-extractor/pkg/scraper"
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
 
 func main() {
+	// Make logs human friendly
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 	log.Info().Msg("Starting...")
+	if ghapi.CanUseAuth() {
+		log.Info().Msg("Will use detected pat for gh api calls")
+	}
 	retriever := reporetriever.NewRankingRetriever()
 	retriever.Scrape()
-
 	scraper.ScrapeAllDBContents()
 }
